c2a46b02ea5c57ccff570e207ae2cfedd5ae9499
angie-ortiz <angie.ortiz@ms223.org>
2016-09-29 14:03:27 -0400

    testing

---
+++ bouncyball/sketch.js
@@ -0,0 +1,14 @@
+var xpos=0
+var xdir=1
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+}
+
+function draw() {
+  background('white');
+  fill('green');
+  ellipse(50,50,50,50);
+  
+  
+}
