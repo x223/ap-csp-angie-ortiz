2feef870662cf4bf043f5dc3a40d55b729b43644
angie-ortiz <angie.ortiz@ms223.org>
2016-10-13 14:21:56 -0400

    Add files via upload

---
+++ AngieOelseif/sketch.js
@@ -0,0 +1,16 @@
+var fillcolor= 'green'
+function setup() {
+  createCanvas(windowWidth,windowHeight)
+}
+
+function draw() {
+  background('white');
+  rect(mouseX,mouseY,200,200);
+  } if (mouseIsPressed) {
+    fillcolor= ('#6638F0');
+  } else {
+    fillcolor= ('#E12874');
+}
+
+
+// need to get the color to change
