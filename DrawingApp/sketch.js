63588de8c87fd16952e3435cb7a48562615761a5
angie-ortiz <angie.ortiz@ms223.org>
2016-10-18 14:25:01 -0400

    Add files via upload

--- DrawingApp/sketch.js
+++ DrawingApp/sketch.js
@@ -3,6 +3,24 @@ var weight= 10
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
+  textSize(10);
+  fill(51)
+  text("Key 'b'= Black", 10, 30);
+  textSize(10);
+  fill('#006B3C')
+  text("Key 'g'= Green", 10, 40);
+  textSize(10);
+  fill('#E32173');
+  text("Key 'p'= Pink", 10, 50);
+  textSize(10);
+  fill('rgb(100%,0%,10%)')
+  text("Key 'r'= Red", 10, 60);
+  textSize(10);
+  fill(255, 204, 0);
+  text("Key 'y'= Yellow", 10, 70);
+  textSize(10);
+  fill(0, 102, 153);
+  text("Key 'e'= Eraser", 10, 80);
 }
 
 function draw() {
