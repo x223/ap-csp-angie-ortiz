b53e37b5bfea6d300a56381bc3e7160c68f2b23b
angie-ortiz <angie.ortiz@ms223.org>
2016-12-20 14:20:07 -0500

    Add files via upload

--- Clicker/sketch.js
+++ Clicker/sketch.js
@@ -1,26 +1,26 @@
 var button;
 score = 0
+
 function setup() {
   createCanvas(400, 300);
   button = createButton("Click Me");
-  button.position(width/2,height/2);
+  button.position(width / 2, height / 2);
   button.mousePressed(randomLocation)
 }
 
-function draw() { 
- 
-}  
+function draw() {}
 
 
 
 function randomLocation() {
-	background(random(255), random(255), random(255));
-	button.position(random(width),random(height));
+  background(random(255), random(255), random(255));
+  button.position(random(width), random(height));
   textSize(20);
-	text("score: " + score , 10, 30);
-	fill('black');
+  text("score: " + score, 10, 30);
+  fill('black');
   score = score + 1
-} 
-
-
-// 
+  if (score > 15) {
+    textSize(20);
+    text("Winner ", 200, 150)
+  }
+}
